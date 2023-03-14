510505f5efa25326597a39ca01e1d94abeae3740
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-14 16:11:29 -0300

    Implementação da camada handler

---
+++ src/main/java/com/cliente/ws/wsrasmooplus/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.cliente.ws.wsrasmooplus.exception;
+
+public class NotFoundException extends RuntimeException{
+    
+    public NotFoundException(String msg){
+        super(msg);
+    }
+}
