731960d81e8c40120da12e16e606e6e1ff02c7b2
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-21 15:22:04 -0300

    Criação e tratamento da BadRequestException

--- src/main/java/com/cliente/ws/wsrasmooplus/handler/ResourceHandler.java
+++ src/main/java/com/cliente/ws/wsrasmooplus/handler/ResourceHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.cliente.ws.wsrasmooplus.exception.BadRequestException;
 import com.cliente.ws.wsrasmooplus.exception.NotFoundException;
 
 @RestControllerAdvice
@@ -13,5 +14,10 @@ public class ResourceHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> notFoundException(NotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage()); 
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> badRequestException(BadRequestException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
--- src/main/java/com/cliente/ws/wsrasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/cliente/ws/wsrasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cliente.ws.wsrasmooplus.service.impl;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.cliente.ws.wsrasmooplus.dto.SubscriptionTypeDto;
+import com.cliente.ws.wsrasmooplus.exception.BadRequestException;
 import com.cliente.ws.wsrasmooplus.exception.NotFoundException;
 import com.cliente.ws.wsrasmooplus.model.SubscriptionType;
 import com.cliente.ws.wsrasmooplus.repository.SubscriptionTypeRepository;
@@ -17,7 +19,7 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     private final SubscriptionTypeRepository subscriptionTypeRepository;
 
-    SubscriptionTypeServiceImpl(SubscriptionTypeRepository subscriptionTypeRepository){
+    SubscriptionTypeServiceImpl(SubscriptionTypeRepository subscriptionTypeRepository) {
         this.subscriptionTypeRepository = subscriptionTypeRepository;
     }
 
@@ -29,7 +31,7 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
     @Override
     public SubscriptionType findById(long id) {
         Optional<SubscriptionType> subscriptionType = subscriptionTypeRepository.findById(id);
-        if (subscriptionType.isPresent()){
+        if (subscriptionType.isPresent()) {
             return subscriptionType.get();
         }
         throw new NotFoundException("SubscriptionType não encontrado!");
@@ -37,13 +39,16 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public SubscriptionType create(SubscriptionTypeDto dto) {
+        if (Objects.nonNull(dto.getId())) {
+            throw new BadRequestException("Id deve ser nulo!");
+        }
         return subscriptionTypeRepository.save(SubscriptionType.builder()
-        .id(dto.getId())
-        .name(dto.getName())
-        .accessMonth(dto.getAcessMonth())
-        .price(dto.getPrice())
-        .productKey(dto.getProductKey())
-        .build());
+                .id(dto.getId())
+                .name(dto.getName())
+                .accessMonth(dto.getAcessMonth())
+                .price(dto.getPrice())
+                .productKey(dto.getProductKey())
+                .build());
     }
 
     @Override
@@ -57,5 +62,5 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
-    
+
 }
