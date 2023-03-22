b80b75365d26f7f35292d03585b8b756b785ca43
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-22 13:06:46 -0300

    Tratamento da MethodArgumentoNotValidException

--- src/main/java/com/cliente/ws/wsrasmooplus/handler/ResourceHandler.java
+++ src/main/java/com/cliente/ws/wsrasmooplus/handler/ResourceHandler.java
@@ -1,7 +1,13 @@
 package com.cliente.ws.wsrasmooplus.handler;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,5 +34,21 @@ public class ResourceHandler {
         .httpStatus(HttpStatus.BAD_REQUEST)
         .code(HttpStatus.BAD_REQUEST.value())
         .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String,String> messages = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error ->{
+            String field = ((FieldError) error).getField();
+            String defaultMessage = error.getDefaultMessage();
+            messages.put(field,defaultMessage);
+        });
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+        .message(Arrays.toString(messages.entrySet().toArray()))
+        .httpStatus(HttpStatus.BAD_REQUEST)
+        .code(HttpStatus.BAD_REQUEST.value())
+        .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
     }
 }
